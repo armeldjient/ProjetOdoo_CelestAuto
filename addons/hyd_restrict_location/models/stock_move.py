6c16713c3ce8717c7f6f6fcb6393ab1d5f874a63
zaza121 <hamza.yaya121@gmail.com>
2023-05-29 07:15:14 +0100

    [FIX] Fix hyd_restrict_location addon
    the function did not have a return statement necessary to call the next inherit method

--- addons/hyd_restrict_location/models/stock_move.py
+++ addons/hyd_restrict_location/models/stock_move.py
@@ -1,8 +1,8 @@
-from odoo import _, models
+from odoo import _, api, models
 from odoo.exceptions import ValidationError
 
 TEMP_MOVE_RESTRICT = _(
-    """Only the users: %s \n """ """are allowed to do transfert %s this location"""
+    """Only the users: %s \n """ """are allowed to do transfert %s this location\n"""
 )
 
 
@@ -13,16 +13,26 @@ class StockMove(models.Model):
 
     def _action_done(self, cancel_backorder=False):
         """."""
+        messages = self.mapped(lambda x: x.check_access_right())
+        error_messages = list(filter(lambda elt: elt, messages))
+        if len(error_messages) > 0:
+            raise ValidationError(error_messages[0])
+        return super(StockMove, self)._action_done(cancel_backorder)
+
+
+    @api.model
+    def check_access_right(self):
+        message = []
         uid = self.env.user.id
-        for record in self:
-            srcl = record.location_id
-            dstl = record.location_dest_id
+        srcl = self.location_id
+        dstl = self.location_dest_id
 
-            if srcl.allowed_users and uid not in srcl.allowed_users.ids:
-                allowed = ",".join(srcl.allowed_users.mapped("name"))
-                raise ValidationError(TEMP_MOVE_RESTRICT % (allowed, "from"))
+        if srcl.allowed_users and uid not in srcl.allowed_users.ids:
+            allowed = ",".join(srcl.allowed_users.mapped("name"))
+            message.append(TEMP_MOVE_RESTRICT % (allowed, "from"))
 
-            if dstl.allowed_users and uid not in dstl.allowed_users.ids:
-                allowed = ",".join(dstl.allowed_users.mapped("name"))
-                raise ValidationError(TEMP_MOVE_RESTRICT % (allowed, "to"))
-            super(StockMove, record)._action_done(cancel_backorder)
+        if dstl.allowed_users and uid not in dstl.allowed_users.ids:
+            allowed = ",".join(dstl.allowed_users.mapped("name"))
+            message.append(TEMP_MOVE_RESTRICT % (allowed, "to"))
+
+        return message[0] if len(message) > 0 else False
